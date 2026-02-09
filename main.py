98398f550e4a61bada6ba42ce5b93de2f661671f
ibrahim ahmed <ibrahema.ussen@gmail.com>
2026-02-09 14:58:49 +0300

    start

--- main.py
+++ main.py
@@ -1,41 +1,5 @@
-from dotenv import load_dotenv
-
-load_dotenv()
-
-from langchain.agents import create_agent
-from langchain_ollama import ChatOllama
-from langchain_tavily import TavilySearch
-
-from schemas import AgentResponse
-
-tools = [TavilySearch()]
-llm = ChatOllama(
-    model="lfm2.5-thinking",
-    temperature=0.7,
-)
-
-
-agent = create_agent(
-    model=llm,
-    tools=tools,
-    response_format=AgentResponse,
-)
-
-
 def main():
-    result = agent.invoke(
-        {
-            "messages": [
-                {
-                    "role": "user",
-                    "content": "search for 3 job postings for an ai engineer using langchain in the bay area on linkedin and list their details",
-                }
-            ]
-        }
-    )
-    # Access structured response from the agent
-    structured = result.get("structured_response", None)
-    print(structured if structured is not None else result)
+  print("Hello, World!")
 
 
 if __name__ == "__main__":
